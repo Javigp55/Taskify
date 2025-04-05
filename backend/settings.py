5479b04c127823eb60f80f471eda5644919968df
Skunk <javigp1995@gmail.com>
2025-04-06 01:33:50 +0200

    dada

--- backend/settings.py
+++ backend/settings.py
@@ -157,15 +157,15 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-# CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
 
-CORS_ALLOWED_ORIGINS = [
-    'https://localhost:5173', 
-    'https://127.0.0.1:5173',
-    'https://vercel.app',
-    'https://javigp55.github.io/',
-]
+# CORS_ALLOWED_ORIGINS = [
+#     'https://localhost:5173', 
+#     'https://127.0.0.1:5173',
+#     'https://vercel.app',
+#     'https://javigp55.github.io/',
+# ]
 
 
 
